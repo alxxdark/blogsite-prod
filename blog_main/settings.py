a9d9aa2e4d9b5405a6e756fc028c28bcedcdb1b8
Ali Sağnak <sagnak.1903@outlook.com>
2025-06-19 18:41:36 +0300

    Whitenoise added, Static Root eklendi

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -43,9 +44,11 @@ INSTALLED_APPS = [
     "crispy_bootstrap4",
     "dashboards",
     
+    
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -123,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR /"staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [BASE_DIR / 'blog_main' / 'static']
 
 # Default primary key field type
