3cab190b3b9e6cc33da8fdf72a90bc54f38bf91c
Ali Sağnak <sagnak.1903@outlook.com>
2025-08-10 18:15:41 +0300

    aaa

--- blogs/apps.py
+++ blogs/apps.py
@@ -1,16 +1,8 @@
 from django.apps import AppConfig
 
-
-class BlogsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'blogs'
-
 class BlogsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'blogs'
 
     def ready(self):
-        import blogs.signals
-
-
-      
+        import blogs.signals  # sinyalleri tek yerden yükle
