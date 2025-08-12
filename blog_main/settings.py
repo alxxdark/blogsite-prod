8d45c542699befb8ab86f07e3f9180bf2e1c23c7
Ali Sağnak <sagnak.1903@outlook.com>
2025-08-12 18:09:08 +0300

    gel

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -105,8 +105,8 @@ if CLOUDINARY_URL:
 else:
     # Lokal geliştirme için
     MEDIA_URL = "/media/"
-    MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-    
+    MEDIA_ROOT = BASE_DIR / "media"
+
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # ========= EMAIL SETTINGS =========
