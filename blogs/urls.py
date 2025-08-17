48939b4e6ec6547b3748e8ee75612cd44afc8dfe
Ali Sağnak <sagnak.1903@outlook.com>
2025-08-17 22:31:48 +0300

    aaaa

--- blogs/urls.py
+++ blogs/urls.py
@@ -2,25 +2,23 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("<int:category_id>/", views.posts_by_category, name="posts_by_category"),
+    # --- Önce spesifik yollar ---
+    path('profile/edit/', views.profile_edit, name='profile_edit'),
+    path('profile/<str:username>/', views.profile_view, name='profile'),
 
-    # Blog detay
+    # Blog detay ve aksiyonlar
+    path('blog/<slug:slug>/save/', views.toggle_save_post, name='toggle_save_post'),
     path('blog/<slug:slug>/', views.blogs, name='blogs'),
-
-    # Beğeni (POST + AJAX)
     path('comment/<int:comment_id>/like/', views.like_comment, name='like_comment'),
     path('like/<slug:slug>/', views.like_post, name='like_post'),
 
-    # KAYDET / KALDIR
-    path('blog/<slug:slug>/save/', views.toggle_save_post, name='toggle_save_post'),
-
-    # Diğerleri
+    # Diğer sayfalar
     path('about/', views.about, name='about'),
-    path('profile/<str:username>/', views.profile_view, name='profile'),
     path('contact/', views.contact_view, name='contact'),
 
-    # En sonda statik sayfa
-    path('<slug:slug>/', views.static_page, name='static_page'),
+    # Kategori (istersen bunu 'category/<int:category_id>/' yapman çakışmaları iyice engeller)
+    path('<int:category_id>/', views.posts_by_category, name='posts_by_category'),
 
-    path('profile/edit/', views.profile_edit, name='profile_edit'),
+    # --- EN SON: catch-all statik sayfa ---
+    path('<slug:slug>/', views.static_page, name='static_page'),
 ]
