2e216a48b675bfc86660f0ca8050331d8d26b3d3
Ali Sağnak <sagnak.1903@outlook.com>
2025-08-27 14:49:20 +0300

    aaa

--- blogs/urls.py
+++ blogs/urls.py
@@ -2,25 +2,25 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("<int:category_id>/", views.posts_by_category, name="posts_by_category"),
+    # Kategoriler
+    path("category/<int:category_id>/", views.posts_by_category, name="posts_by_category"),
 
     # Blog detay
-    path('blog/<slug:slug>/', views.blogs, name='blogs'),
+    path("blog/<slug:slug>/", views.blogs, name="blogs"),
 
-    # Beğeni (POST + AJAX)
-    path('comment/<int:comment_id>/like/', views.like_comment, name='like_comment'),
-    path('like/<slug:slug>/', views.like_post, name='like_post'),
+    # Yorum / post beğeni
+    path("comment/<int:comment_id>/like/", views.like_comment, name="like_comment"),
+    path("like/<slug:slug>/", views.like_post, name="like_post"),
 
-    # KAYDET / KALDIR
-    path('blog/<slug:slug>/save/', views.toggle_save_post, name='toggle_save_post'),
+    # Kaydet / kaldır
+    path("blog/<slug:slug>/save/", views.toggle_save_post, name="toggle_save_post"),
 
-    # Diğerleri
-    path('about/', views.about, name='about'),
-    path('profile/<str:username>/', views.profile_view, name='profile'),
-    path('contact/', views.contact_view, name='contact'),
+    # Diğer sayfalar
+    path("about/", views.about, name="about"),
+    path("profile/<str:username>/", views.profile_view, name="profile"),
+    path("profile/edit/", views.profile_edit, name="profile_edit"),
+    path("contact/", views.contact_view, name="contact"),
 
-    # En sonda statik sayfa
-    path('<slug:slug>/', views.static_page, name='static_page'),
- 
-    path('profile/edit/', views.profile_edit, name='profile_edit'),
+    # En sonda: statik sayfalar (slug)
+    path("<slug:slug>/", views.static_page, name="static_page"),
 ]
